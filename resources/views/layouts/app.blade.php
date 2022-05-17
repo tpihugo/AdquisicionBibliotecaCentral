a20cba414aa91a2e80be05bc3463141e35e2b912
Yohan Chayan <yohan.chayan7502@alumnos.udg.mx>
2022-05-17 14:13:11 -0500

    registro usuario sin permisos habilitado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,7 +92,7 @@
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    {{-- <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a> --}}
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
                                 </li>
                             @endif
                         @else
